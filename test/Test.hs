f4e31e6bc5caf43a6ae12776cbda08a05f25bb7e
Chris Mears <chris@cmears.id.au>
2016-03-14 09:33:32 +1100

    Better testing

--- test/Test.hs
+++ test/Test.hs
@@ -1,7 +1,9 @@
+import Control.Exception
 import Control.Monad
 import qualified Data.ByteString.Lazy as BSL
 import System.FilePath
 import System.IO
+import System.IO.Error
 import System.IO.Temp hiding (openNewBinaryFile)
 import System.Posix
 import System.Random
@@ -17,9 +19,22 @@ main = do
   test5
   test6
   test7
+  test8
 
-inTempDirectory :: FilePath -> String -> IO () -> IO ()
-inTempDirectory path pattern action = do
+-- Wrapper around inTempDirectory' which uses a default path and
+-- pattern, and handles exceptions such that the temporary directory
+-- is always removed.
+inTempDirectory :: IO () -> IO ()
+inTempDirectory action = do
+  inTempDirectory' "." "tempXXX"
+    (catchIOError action (\e -> do
+      hPutStrLn stderr (show e)
+      return ()))
+
+-- Wrapper around withTempDirectory which changes into the directory
+-- to run the action, then restores the previous working directory.
+inTempDirectory' :: FilePath -> String -> IO () -> IO ()
+inTempDirectory' path pattern action = do
   olddir <- getWorkingDirectory
   withTempDirectory path pattern $ \tempdir -> do
     changeWorkingDirectory tempdir
@@ -29,79 +44,91 @@ inTempDirectory path pattern action = do
 -- Simple test: make sure we can find a witness.
 test1 :: IO ()
 test1 = do
-  inTempDirectory "." "tempXXX" $ do
+  inTempDirectory $ do
     createDirectory "dir" ownerModes
     createRandomFile ("dir" </> "abc")
     copyFile ("dir" </> "abc") "def"
     shouldBeJust =<< findWitness False "def" "dir"
-  putStrLn "test1 OK"
+    putStrLn "test1 OK"
 
 -- Make sure that file contents are checked.  (Makes a proper witness,
 -- but then alters one byte so they don't match.)
 test2 :: IO ()
 test2 = do
-  inTempDirectory "." "tempXXX" $ do
+  inTempDirectory $ do
     createDirectory "dir" ownerModes
     createRandomFile ("dir" </> "abc")
     createLink ("dir" </> "abc") ("dir" </> "link")
     copyFile ("dir" </> "abc") "def"
     alterOneByte "def"
     shouldBeNothing =<< findWitness False "def" "dir"
-  putStrLn "test2 OK"
+    putStrLn "test2 OK"
 
 -- Make sure we don't use a symbolic link as a witness.
 test3 :: IO ()
 test3 = do
-  inTempDirectory "." "tempXXX" $ do
+  inTempDirectory $ do
     createDirectory "dir" ownerModes
     createRandomFile "abc"
     createSymbolicLink (".." </> "abc") ("dir" </> "link")
     copyFile "abc" "def"
     shouldBeNothing =<< findWitness False "def" "dir"
-  putStrLn "test3 OK"
+    putStrLn "test3 OK"
 
 -- Make sure we don't use a hard link as a witness.
 test4 :: IO ()
 test4 = do
-  inTempDirectory "." "tempXXX" $ do
+  inTempDirectory $ do
     createDirectory "dir" ownerModes
     createRandomFile "abc"
     createLink "abc" ("dir" </> "link")
     shouldBeNothing =<< findWitness False "abc" "dir"
-  putStrLn "test4 OK"
+    putStrLn "test4 OK"
 
 -- Make sure we don't follow symlinks to directories during witness
 -- search.
 test5 :: IO ()
 test5 = do
-  inTempDirectory "." "tempXXX" $ do
+  inTempDirectory $ do
     createDirectory "dir" ownerModes
     createRandomFile ("dir" </> "abc")
     createDirectory "base" ownerModes
     createSymbolicLink (".." </> "dir" </> "abc") ("base" </> "link")
     copyFile ("dir" </> "abc") "target"
     shouldBeNothing =<< findWitness False "target" "base"
-  putStrLn "test5 OK"
+    putStrLn "test5 OK"
 
 -- Make sure we don't use a file as its own witness.
 test6 :: IO ()
 test6 = do
-  inTempDirectory "." "tempXXX" $ do
+  inTempDirectory $ do
     createDirectory "dir" ownerModes
     createRandomFile ("dir" </> "abc")
     shouldBeNothing =<< findWitness False ("dir" </> "abc") "dir"
-  putStrLn "test6 OK"
+    putStrLn "test6 OK"
 
 -- Make sure fast checking doesn't look at file contents.
 test7 :: IO ()
 test7 = do
-  inTempDirectory "." "tempXXX" $ do
+  inTempDirectory $ do
     createDirectory "dir" ownerModes
     createRandomFile ("dir" </> "abc")
     createRandomFile "abc"
     shouldBeJust =<< findWitness True "abc" "dir"
-  putStrLn "test7 OK"
-    
+    putStrLn "test7 OK"
+
+-- Make sure expandTarget gives the right result.
+test8 :: IO ()
+test8 = do
+  inTempDirectory $ do
+    createDirectory "dir" ownerModes
+    createRandomFile ("dir" </> "abc")
+    createSymbolicLink "abc" ("dir" </> "link")
+    createDirectory ("dir" </> "dir2") ownerModes
+    createRandomFile ("dir" </> "dir2" </> "def")
+    paths <- expandTarget "dir"
+    shouldBeEqual paths ["dir/abc","dir/dir2/def"]
+    putStrLn "test8 OK"
 
 -- Creates a file of 1024 random bytes at the given path.
 createRandomFile :: FilePath -> IO ()
@@ -128,19 +155,24 @@ openNewBinaryFile :: FilePath -> IO Handle
 openNewBinaryFile path = do
   exists <- fileExist path
   if exists
-    then error ("openNewBinaryFile: already exists: " ++ path)
+    then throwIO (userError ("openNewBinaryFile: already exists: " ++ path))
     else openBinaryFile path WriteMode
 
 shouldBeNothing :: Show a => Maybe a -> IO ()
 shouldBeNothing Nothing = return ()
 shouldBeNothing (Just x) = do
-  error ("error: shouldBeNothing: " ++ show (Just x))
+  throwIO (userError ("error: shouldBeNothing: " ++ show (Just x)))
   
 shouldBeJust :: Maybe a -> IO ()
 shouldBeJust (Just _x) = return ()
 shouldBeJust Nothing = do
-  error ("error: shouldBeJust: Nothing")
-  
+  throwIO (userError ("error: shouldBeJust: Nothing"))
+
+shouldBeEqual :: (Eq a) => a -> a -> IO ()
+shouldBeEqual x y =
+  if x == y
+  then return ()
+  else throwIO (userError ("error: shouldBeEqual: not equal"))
 
 -- Change one byte in a file so that the result is guaranteed to be
 -- different from before.  File must have at least 101 bytes for this
